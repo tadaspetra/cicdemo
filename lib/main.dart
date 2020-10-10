f26669872b83f2fc4e4a2b03bb0313e282f1a039
Tadas Petra <connectwithamateur@gmail.com>
2020-10-09 19:56:17 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Trigger 2',
+      title: 'Flutter Trigger 3',
       theme: ThemeData(
         // This is the theme of your application.
         //
